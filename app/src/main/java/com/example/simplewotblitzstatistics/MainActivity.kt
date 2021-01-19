2623905cd8dadfdb035fd35f183f9ec815f6c5af
Majesta <Vasiliy23898@gmail.com>
2021-01-19 20:52:15 +0300

    small improvements

--- app/src/main/java/com/example/simplewotblitzstatistics/MainActivity.kt
+++ app/src/main/java/com/example/simplewotblitzstatistics/MainActivity.kt
@@ -11,59 +11,68 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NicknameDialog.INicknameListener {
-    var nickname: String = ""
+    private var nickname: String = ""
+    private val dataController = DataController()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nickname = DataController().getString("NICKNAME", this)
+        nickname = dataController.getString("NICKNAME", this)
 
         if (nickname.isEmpty()) {
             calc_session.isEnabled = false
             openDialog()
         }
 
-        val dataController = DataController()
-
         calc_session.setOnClickListener {
             if (isOnline()) {
                 when (calc_session.text) {
                     "Start Calc Session" -> {
-                        tanks.removeAllViews()
-                        dataController.setPlayer(nickname)
-                        val state = dataController.getStartState()
-
-                        if (state) {
-                            LayoutManager().getToast("calculation of your session statistics started", this)
-                            calc_session.text = "Stop Calc Session"
-                            calc_session.setIconResource(android.R.drawable.ic_media_pause)
-                        } else {
-                            LayoutManager().getToast("no player with this nickname", this)
-                        }
+                        handleStartSession()
                     }
                     "Stop Calc Session" -> {
-                        val tanksStatisticsData = dataController.stopSession()
-
-                        tanksStatisticsData.forEach {
-                            val view = LayoutManager().getTankStatisticsView(
-                                tankTitle = it.first,
-                                tankAvg = it.second,
-                                tankImageUrl = it.third,
-                                layout = tanks,
-                                context = this)
-                            tanks.addView(view)
-                        }
-
-                        calc_session.text = "Start Calc Session"
-                        calc_session.setIconResource(android.R.drawable.ic_media_play)
+                        handleStopSession()
                     }
                 }
             } else {
                 LayoutManager().getToast("no internet connection", this)
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleStartSession() {
+        tanks.removeAllViews()
+        dataController.setPlayer(nickname)
+        val state = dataController.getStartState()
+
+        if (state) {
+            LayoutManager().getToast("calculation of your session statistics started", this)
+            calc_session.text = "Stop Calc Session"
+            calc_session.setIconResource(android.R.drawable.ic_media_pause)
+        } else {
+            LayoutManager().getToast("no player with this nickname", this)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleStopSession() {
+        val tanksStatisticsData = dataController.stopSession()
+
+        tanksStatisticsData.forEach {
+            val view = LayoutManager().getTankStatisticsView(
+                tankTitle = it.first,
+                tankAvg = it.second,
+                tankImageUrl = it.third,
+                layout = tanks,
+                context = this)
+            tanks.addView(view)
+        }
+
+        calc_session.text = "Start Calc Session"
+        calc_session.setIconResource(android.R.drawable.ic_media_play)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -88,7 +97,7 @@ class MainActivity : AppCompatActivity(), NicknameDialog.INicknameListener {
     override fun applyText(nickname: String) {
         if (nickname.isNotBlank()) {
             this.nickname = nickname
-            DataController().saveString("NICKNAME", nickname, this)
+            dataController.saveString("NICKNAME", nickname, this)
             calc_session.isEnabled = true
         }
     }
